c526e83a5f8da08adfbf616c3093894b72af1e1b
Fujikouki <c1202599@st.kanazawa-it.ac.jp>
2025-10-01 19:57:27 +0900

    ナビゲーションの初期設定とメイン画面の実装

--- app/src/main/java/com/example/kouki/fujisue/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/kouki/fujisue/androidlab/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.kouki.fujisue.androidlab.ui.main.MainScreen
+import com.example.kouki.fujisue.androidlab.ui.navigation.Route
 import com.example.kouki.fujisue.androidlab.ui.theme.AndroidLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,77 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AndroidLabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppContent()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppContent() {
+    AndroidLabTheme {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = Route.Main) {
+            composable<Route.Main> {
+                MainScreen(navController = navController)
+            }
+            composable<Route.TextScreen> {
+                // TODO: TextScreenを実装する
+                Text("TextScreen")
+            }
+            composable<Route.ButtonScreen> {
+                // TODO: ButtonScreenを実装する
+                Text("ButtonScreen")
+            }
+            composable<Route.ImageScreen> {
+                // TODO: ImageScreenを実装する
+                Text("ImageScreen")
+            }
+            composable<Route.ListScreen> {
+                // TODO: ListScreenを実装する
+                Text("ListScreen")
+            }
+            composable<Route.DialogScreen> {
+                // TODO: DialogScreenを実装する
+                Text("DialogScreen")
+            }
+            composable<Route.InputScreen> {
+                // TODO: InputScreenを実装する
+                Text("InputScreen")
+            }
+            composable<Route.LayoutsScreen> {
+                // TODO: LayoutsScreenを実装する
+                Text("LayoutsScreen")
+            }
+            composable<Route.StateManagementScreen> {
+                // TODO: StateManagementScreenを実装する
+                Text("StateManagementScreen")
+            }
+            composable<Route.PermissionsScreen> {
+                // TODO: PermissionsScreenを実装する
+                Text("PermissionsScreen")
+            }
+            composable<Route.NetworkingScreen> {
+                // TODO: NetworkingScreenを実装する
+                Text("NetworkingScreen")
+            }
+            composable<Route.StorageScreen> {
+                // TODO: StorageScreenを実装する
+                Text("StorageScreen")
+            }
+            composable<Route.AnimationScreen> {
+                // TODO: AnimationScreenを実装する
+                Text("AnimationScreen")
+            }
+            composable<Route.ThemingScreen> {
+                // TODO: ThemingScreenを実装する
+                Text("ThemingScreen")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AndroidLabTheme {
-        Greeting("Android")
-    }
+fun DefaultPreview() {
+    AppContent()
 }
---
+++ app/src/main/java/com/example/kouki/fujisue/androidlab/ui/main/MainScreen.kt
@@ -0,0 +1,76 @@
+package com.example.kouki.fujisue.androidlab.ui.main
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.kouki.fujisue.androidlab.ui.navigation.Route
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(navController: NavController) {
+    val screens = listOf(
+        Route.TextScreen to "テキスト表示を試す画面",
+        Route.ButtonScreen to "ボタンのインタラクションを試す画面",
+        Route.ImageScreen to "画像の表示を試す画面",
+        Route.ListScreen to "リスト表示を試す画面",
+        Route.DialogScreen to "ダイアログ表示を試す画面",
+        Route.InputScreen to "ユーザー入力を試す画面 (TextField, Checkbox, Switch, Sliderなど)",
+        Route.LayoutsScreen to "Jetpack Composeのレイアウト方法を学ぶ画面 (Column, Row, Box, ConstraintLayoutなど)",
+        Route.StateManagementScreen to "状態管理を試す画面 (ViewModel, remember, LaunchedEffectなど)",
+        Route.PermissionsScreen to "実行時パーミッションの扱い方を学ぶ画面",
+        Route.NetworkingScreen to "ネットワークリクエストとデータ表示を学ぶ画面",
+        Route.StorageScreen to "データ永続化 (SharedPreferences, Room) を学ぶ画面",
+        Route.AnimationScreen to "Jetpack Composeのアニメーションを試す画面",
+        Route.ThemingScreen to "アプリのテーマ設定 (ライト/ダークテーマ、ダイナミックカラー) を学ぶ画面"
+    )
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Android Lab - メインメニュー") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item {
+                Text(
+                    text = "このアプリは、Android開発における様々なUIコンポーネントや機能を試すための学習用アプリです。各項目を選択して、動作を確認してみてください。",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+            }
+            items(screens) { (route, name) ->
+                Button(
+                    onClick = { navController.navigate(route) },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(name)
+                }
+            }
+        }
+    }
+}
