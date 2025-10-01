e4f0754f896b376ca80797ab0c287441f3dff270
Fujikouki <c1202599@st.kanazawa-it.ac.jp>
2025-10-01 19:48:00 +0900

    各学習項目へのナビゲーションルートを定義する

---
+++ app/src/main/java/com/example/kouki/fujisue/androidlab/ui/navigation/Route.kt
@@ -0,0 +1,62 @@
+package com.example.kouki.fujisue.androidlab.ui.navigation
+
+import kotlinx.serialization.Serializable
+
+object Route {
+
+    /** メイン画面 */
+    @Serializable
+    data object Main
+
+    /** テキスト表示を試す画面 */
+    @Serializable
+    data object TextScreen
+
+    /** ボタンのインタラクションを試す画面 */
+    @Serializable
+    data object ButtonScreen
+
+    /** 画像の表示を試す画面 */
+    @Serializable
+    data object ImageScreen
+
+    /** リスト表示を試す画面 */
+    @Serializable
+    data object ListScreen
+
+    /** ダイアログ表示を試す画面 */
+    @Serializable
+    data object DialogScreen
+
+    /** ユーザー入力を試す画面 (TextField, Checkbox, Switch, Sliderなど) */
+    @Serializable
+    data object InputScreen
+
+    /** Jetpack Composeのレイアウト方法を学ぶ画面 (Column, Row, Box, ConstraintLayoutなど) */
+    @Serializable
+    data object LayoutsScreen
+
+    /** 状態管理を試す画面 (ViewModel, remember, LaunchedEffectなど) */
+    @Serializable
+    data object StateManagementScreen
+
+    /** 実行時パーミッションの扱い方を学ぶ画面 */
+    @Serializable
+    data object PermissionsScreen
+
+    /** ネットワークリクエストとデータ表示を学ぶ画面 */
+    @Serializable
+    data object NetworkingScreen
+
+    /** データ永続化 (SharedPreferences, Room) を学ぶ画面 */
+    @Serializable
+    data object StorageScreen
+
+    /** Jetpack Composeのアニメーションを試す画面 */
+    @Serializable
+    data object AnimationScreen
+
+    /** アプリのテーマ設定 (ライト/ダークテーマ、ダイナミックカラー) を学ぶ画面 */
+    @Serializable
+    data object ThemingScreen
+}
